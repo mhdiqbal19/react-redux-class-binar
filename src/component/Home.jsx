14d6792b6fd5c867bf646f2396c963f695a5bd4c
MUHAMMAD IQBAL <43297852+mhdiqbal19@users.noreply.github.com>
2022-09-01 20:05:22 +0700

    first commit

---
+++ src/component/Home.jsx
@@ -0,0 +1,15 @@
+import { Component } from "react";
+
+class Home extends Component {
+
+    state = {  }
+    render() { 
+        return ( 
+            <div>
+                <h1>Hai</h1>
+            </div>
+         );
+    }
+}
+ 
+export default Home;
---
+++ src/redux/reducers/dataReducer.jsx
@@ -0,0 +1,10 @@
+const initialState = {
+    data: [],
+}
+
+const data = (state = initialState, action) => {
+    return state;
+}
+
+
+export default data;
